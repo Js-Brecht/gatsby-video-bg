02b74333509c5a05a137a07c696ae4fc46b1cace
Jeremy Albright <myrlin1@gmail.com>
2020-03-24 12:48:22 -1000

    init

---
+++ docs/styles-e0a23504e2cfe09eeb83.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,p,w){}}]);
+//# sourceMappingURL=styles-e0a23504e2cfe09eeb83.js.map
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,36 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-const fs = require('fs');
-var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)|[^(\S]*([^()=]*)[\s)]*\=\>/m;
-var FN_ARG_SPLIT = /,/;
-var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-
-function formalParameterList(fn) {
-    var fnText, argDecl;
-    var args = [];
-    fnText = fn.toString().replace(STRIP_COMMENTS, '');
-    argDecl = fnText.match(FN_ARGS);
-
-    if (!argDecl) return []
-    var r = (argDecl[1] || argDecl[2] || "").split(FN_ARG_SPLIT);
-    for (var a in r) {
-        var arg = r[a];
-        arg.replace(FN_ARG, function (all, underscore, name) {
-            args.push(name);
-        });
-    }
-    return args;
-}
-
-exports.onCreateWebpackConfig = ({ actions, getConfig, stage }) => {
-    if (stage !== 'build-html') return;
-    fs.writeFileSync('./webpack.config', JSON.stringify(
-        getConfig(),
-        (key, val) => val instanceof RegExp ? val.source : typeof val === 'function' ? `function ${val.name}(${formalParameterList(val).join(', ')})` : val,
-        3
-    ))
-    process.exit(0)
-}
